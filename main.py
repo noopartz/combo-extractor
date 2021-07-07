620eb4a91e153a0ec5be3cd2eee6f545d4e66527
noopartz <50530195+noopartz@users.noreply.github.com>
2021-07-07 23:59:55 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import re
+import easygui
+
+file = easygui.fileopenbox()
+
+raw = open(file, 'r')
+ready = open('combo.txt', 'w')
+count = 0
+
+
+for line in raw:
+    pieces = line.split(":")
+    email = pieces[0]
+    password = pieces[1]
+    count = count + 1
+    pasw = re.findall(r"[A-Za-z0-9\.\-+_]+[A-Za-z0-9\.\-+_]+", str(password))
+    combo = (str(email)+':'+str(pasw[0]))
+    ready.writelines(combo+'\n')
+
+print('ready')
+
+#written by noopartz
