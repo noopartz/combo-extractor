15949c9214f1bd9782f7079953d9e716700b378c
noopartz <50530195+noopartz@users.noreply.github.com>
2021-07-08 00:00:11 +0200

    Add files via upload

---
+++ requirements installer.py
@@ -0,0 +1,11 @@
+import os
+import time
+
+print('Starting installer')
+time.sleep(0.5)
+os.system('pip install easygui')
+os.system('pip install re')
+os.system('cls')
+print('Ready')
+input('press enter for close')
+exit()
