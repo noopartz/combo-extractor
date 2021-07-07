1611284d76986aeca248d9456d667e9ad34890e1
noopartz <50530195+noopartz@users.noreply.github.com>
2021-07-08 00:13:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ for line in raw:
     pieces = line.split(":")
     email = pieces[0]
     password = pieces[1]
-    count = count + 1
     pasw = re.findall(r"[A-Za-z0-9\.\-+_]+[A-Za-z0-9\.\-+_]+", str(password))
     combo = (str(email)+':'+str(pasw[0]))
     ready.writelines(combo+'\n')
